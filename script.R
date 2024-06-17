0360afaa2a22699f9c3213fe6dc360c617ebc35a
haverstein <haverstein3@gmail.com>
2024-06-17 21:20:17 +0530

    model accuracy

--- script.R
+++ script.R
@@ -1,3 +1,15 @@
+#-0.31	0.41	0.51	-0.20	0.61	0.20	0.61	-0.30	-0.10	-0.20	-0.51	0.41
+#-0.51	0.61	-0.20	0.10	-0.10	0.30	0.00	0.20	-0.30	0.00	-0.10	0.81
+#-0.60	0.40	0.50	0.10	-0.10	0.00	0.20	0.10	-0.10	0.10	0.10	0.60
+#-0.20	0.60	0.59	0.00	0.00	0.10	0.20	0.10	0.20	0.00	0.20	0.19
+#-0.10	0.68	0.58	-0.19	0.00	-0.19	0.39	0.38	0.10	0.00	0.10	0.29
+#-0.48	0.57	0.48	-0.47	0.38	0.09	0.47	0.00	0.00	-0.19	0.19	0.38
+#-0.56	0.47	0.28	-0.19	-0.09	0.28	0.28	0.00	0.00	-0.28	0.00	0.00
+#-1.03	0.85	0.47	0.00	0.09	-0.09	0.19	0.00	-0.19	0.00	0.09	-0.09
+#-0.84	0.38	0.75	-0.37	0.28	0.09	0.28	0.00	0.09	0.19	0.09	0.74
+#-0.64	0.65	0.18	0.00	-0.18	0.18	0.37	0.09	0.09	0.00	
+
+
 data = scan()
 
 germaninfl = ts(data,start=2008,frequency = 12)
@@ -20,8 +32,39 @@ auto.arima(germaninfl,stepwise = T,
            approximation = F,trace = T)
 
 germaninflarima = auto.arima(germaninfl,stepwise = T,
-                             apSproximation = F,trace = T)
+                             approximation = F,trace = T)
 
 # Forecast
 forec = forecast(germaninflarima)
 autoplot(forec)
+
+#Exponential Smoothing
+
+ets(germaninfl)
+
+germaninflets = ets(germaninfl)
+
+plot(forecast(germaninflets,h=60))
+
+# Time Series Cross Validation
+
+forecastets = function(x,h){
+  forecast(ets(x),h=h)
+}
+
+forecastarima = function(x,h){
+  forecast(auto.arima(x),
+           stepwise=T,
+           approximation=F,
+           h=h)
+}
+
+etserror = tsCV(germaninfl,forecastets,h=1)
+arimaerror = tsCV(germaninfl,forecastarima,h=1)
+
+etsmse = mean(etserror^2,na.rm=TRUE)
+arimamse = mean(arimaerror^2,na.rm=TRUE)
+
+names = c('Exponential Smoothing','ARIMA')
+
+barplot(c(etsmse,arimamse),names.arg = names,xlab='Models',ylab='MSE',main='Model Accuracy for different models')
